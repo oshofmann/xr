421c760300f39eb65e0d22b199f71abe8eec9010
Michael Lee <mzlee@cs.utexas.edu>
2011-11-30 16:06:11 -0600

    Made the export a bit more robust.  Also tarring the obj file (not sure this is good)

--- gen/export.sh
+++ gen/export.sh
@@ -1,13 +1,46 @@
-#!/bin/sh -e
-mkdir xr.export
-
+#!/bin/sh
+CHANGED=0
+mkdir -p xr.export
 for file in `cd tree; find . -name "*.xr"`
 do
-	echo $file
 	mkdir -p xr.export/tree/`dirname $file`
-	cp tree/$file xr.export/tree/$file
+	src=tree/${file}
+	dst=xr.export/tree/${file}
+	if [ -f "${src}" -a -f "${dst}" ]
+	then
+		retstr=`diff -q ${src} ${dst}`
+		if [ "x${retstr}" != "x" ]
+		then
+			echo "${file}"
+			cp ${src} ${dst}
+			CHANGED=1
+		fi
+	else
+		echo "${file}"
+		cp ${src} ${dst}
+		CHANGED=1
+	fi
 done
 
-cp -r objs xr.export
+if [ ${CHANGED} -eq 1 ]
+then
+	tar -zcf xr.export/objs.tar.gz objs
+fi
 
-cp ../js/*.js ../js/*.html ../js/*.css xr.export
+for file in `ls ../js/*.js ../js/*.html ../js/*.css`
+do
+	src=${file}
+	dst=xr.export/`basename ${file}`
+	if [ -f "${src}" -a -f "${dst}" ]
+	then
+		retstr=`diff -q ${src} ${dst}`
+		if [ "x${retstr}" != "x" ]
+		then
+			echo "${file}"
+			cp ${src} ${dst}
+		fi
+	else
+		echo "${file}"
+		cp ${src} ${dst}
+	fi
+done
